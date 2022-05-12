d3b611bdec83cd78cd3c1a4d90c53ca58d17e763
create <2302704370@qq.com>
2022-05-12 18:10:05 +0800

    first commit

---
+++ src/main/java/com.atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yjt
+ * @create 2022-05-12-18:06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
